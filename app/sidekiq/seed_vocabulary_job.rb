25d20f0ecc7a8b9b23d453fb6024c342bb179b61
Axel Bagenal <axel.bagenal@giantmonkey.de>
2024-08-03 14:56:13 +0200

    update sidekiq job

--- app/sidekiq/seed_vocabulary_job.rb
+++ app/sidekiq/seed_vocabulary_job.rb
@@ -1,10 +1,29 @@
 class SeedVocabularyJob
   include Sidekiq::Job
 
+  sidekiq_options retry: 5
+
   def perform(batch)
-    Vocabulary.transaction do
-      batch.each do |entry|
-        Vocabulary.create!(word: entry['word'])
+    retries = 0
+    begin
+      ActiveRecord::Base.connection_pool.with_connection do
+        Vocabulary.transaction do
+          batch.each do |entry|
+            Vocabulary.find_or_create_by!(word: entry['word'])
+          end
+        end
+      end
+    rescue ActiveRecord::StatementInvalid => e
+      if e.message =~ /database is locked/
+        retries += 1
+        if retries < 5
+          sleep(0.5 * retries)
+          retry
+        else
+          raise e
+        end
+      else
+        raise e
       end
     end
   end
