b22190c190c2f11897e0abda86596559c120747f
Axel Bagenal <axel.bagenal@giantmonkey.de>
2024-08-03 13:11:11 +0200

    add annotate gem and annotate vocab model

--- app/models/vocabulary.rb
+++ app/models/vocabulary.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: vocabularies
+#
+#  id          :integer          not null, primary key
+#  word        :string
+#  translation :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Vocabulary < ApplicationRecord
   before_create :fetch_translation
 
