c10c1fdf7242c6a0ef4fe20a3780700972de208e
Axel Bagenal <axel.bagenal@giantmonkey.de>
2024-08-03 13:15:09 +0200

    update seed file with vocabulary

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+
+file_path = Rails.root.join('db/seeds/German-words-5000-words.json')
+json_data = JSON.parse(File.read(file_path))
+
+json_data.each do |entry|
+  Vocabulary.create!(word: entry['word'])
+end
