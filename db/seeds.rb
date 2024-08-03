cdb278dbbe3d42967de33e5b9a4ee6b31afa1883
Axel Bagenal <axel.bagenal@giantmonkey.de>
2024-08-03 13:30:46 +0200

    refactor seed file for better data handling
    refactor

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,22 @@
 require 'json'
 
-file_path = Rails.root.join('db/seeds/German-words-5000-words.json')
+file_path = Rails.root.join('db', 'seeds', 'German-words-5000-words.json')
+puts "Reading JSON file..."
 json_data = JSON.parse(File.read(file_path))
 
-json_data.each do |entry|
-  Vocabulary.create!(word: entry['word'])
+batch_size = 100
+total_records = json_data.size
+total_batches = (total_records / batch_size).ceil
+
+puts "Processing #{total_records} records in #{total_batches} batches of #{batch_size} records each..."
+
+json_data.each_slice(batch_size).with_index do |batch, index|
+  Vocabulary.transaction do
+    batch.each do |e|
+      Vocabulary.create!(word: e['word'])
+    end
+  end
+  puts "Processed batch #{index + 1} of #{total_batches}..."
 end
+
+puts "Seeding completed."
