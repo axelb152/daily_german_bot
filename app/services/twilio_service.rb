7010559a7da66e5a28f0016791da7a8b8226a95b
Axel Bagenal <axel.bagenal@giantmonkey.de>
2024-08-03 15:21:04 +0200

    use sandbox credentials for now

--- app/services/twilio_service.rb
+++ app/services/twilio_service.rb
@@ -4,7 +4,8 @@ class TwilioService
   def self.send_whatsapp_message(to, body)
     client = Twilio::REST::Client::new
     client.messages.create(
-      from: "whatsapp:#{ENV['TWILIO_PHONE_NUMBER']}",
+      # from: "whatsapp:#{ENV['TWILIO_PHONE_NUMBER']}",
+      from: "whatsapp:#{ENV['TWILIO_SANDBOX_NUMBER']}", # use sandbox number for now
       to: "whatsapp:#{to}",
       body: body
     )
