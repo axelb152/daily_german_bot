9a536ee69b00f53240f369cc048fb2823b6c3ac4
Axel Bagenal <axel.bagenal@giantmonkey.de>
2024-08-03 14:56:36 +0200

    adjust seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,11 @@ require 'json'
 file_path = Rails.root.join('db', 'seeds', 'German-words-5000-words.json')
 puts "Reading JSON file..."
 json_data = JSON.parse(File.read(file_path))
-puts "Finished reading JSON..."
+puts "Finished reading JSON file."
 
-batch_size = 50
+batch_size = 50  # Smaller batch size for Sidekiq
 total_records = json_data.size
-total_batches = (total_records / batch_size).ceil
+total_batches = (total_records / batch_size.to_f).ceil
 
 puts "Processing #{total_records} records in #{total_batches} batches of #{batch_size} records each..."
 
