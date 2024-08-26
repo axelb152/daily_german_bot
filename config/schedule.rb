7359c6f87fc6d0763f6839a4c719ec49d5c86252
Axel Bagenal <axel.bagenal@gmail.com>
2024-08-26 07:33:44 +0000

    make the rake task cron job executable more specific so that it works in prod

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,6 @@
-set :output, "/root/daily_german_bot/log/cron_log.log"
+set :output, "/root/daily_german_bot/config/log/cron_log.log"
+
+job_type :rake, "cd :path && :environment_variable=:environment /root/.asdf/shims/bundle exec rake :task --silent :output"
 
 every 1.day, at: '10:00 am' do
   rake "daily_reminder:send"
